4e82a50661eae8deadc317ca12b42c198870c8c4
julianit <yulian_iliev@yahoo.com>
2017-01-28 21:35:18 -0800

    Flash tool

---
+++ ConnectToAP.cpp
@@ -0,0 +1,105 @@
+/*
+	I used ESP8266 E12 Node MCU (AMICA)
+	
+	This example set the module in a station mode.
+	Connect to an access point and print its IP Address as soon
+	the ip address is assing. 
+
+*/
+#include <ets_sys.h>
+#include <osapi.h>
+#include "user_interface.h"
+#include <os_type.h>
+#include <gpio.h>
+#include "espconn.h"
+#include "driver/uart.h"
+
+#define DELAY 1000 /* milliseconds */
+
+static ETSTimer network_timer;
+extern int ets_uart_printf(const char *fmt, ...);
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABBBCDDD
+ *                A : rf cal
+ *                B : at parameters
+ *                C : rf init data
+ *                D : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+*******************************************************************************/
+
+uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 8;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void ICACHE_FLASH_ATTR user_rf_pre_init(void)
+{
+}
+
+void ICACHE_FLASH_ATTR network_check_ip(void)
+{
+    struct ip_info ipconfig;
+    os_timer_disarm(&network_timer);
+    wifi_get_ip_info(STATION_IF, &ipconfig);
+	
+    if (wifi_station_get_connect_status() == STATION_GOT_IP && ipconfig.ip.addr != 0)
+    {
+		os_printf("IP found\n");
+    }
+    else
+    {
+		// This is printed until the ESP get an IP Address 
+		os_printf("No IP found\n");
+        os_timer_disarm(&network_timer);
+        os_timer_setfn(&network_timer, (os_timer_func_t *)network_check_ip, NULL);
+        os_timer_arm(&network_timer, DELAY, 0);
+    }
+}
+
+void ICACHE_FLASH_ATTR user_init()
+{
+    // Set AP settings
+    char ssid[32] = "AP-NAME"; // AP name  
+    char password[64] = "AP-PASSWORD"; //AP Password
+    struct station_config stationConf;
+    wifi_set_opmode( STATION_MODE );
+    os_memcpy(&stationConf.ssid, ssid, 32);
+    os_memcpy(&stationConf.password, password, 32);
+    wifi_station_set_config(&stationConf);
+    wifi_station_connect();
+    network_check_ip();
+}
+
